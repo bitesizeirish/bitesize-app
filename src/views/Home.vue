4f7109f6d0acd0de03872aa5c0368c46a2ab9d3e
Eoin Ó Conchúir <e@eoinoc.net>
2018-08-17 06:21:57 +0100

    Add remaining files generated by vue-cli

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Bitesize Irish Gealic logo" src="../assets/bitesize-irish-gaelic.png">
+    <HelloWorld msg="Welcome to the future Bitesize app"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
