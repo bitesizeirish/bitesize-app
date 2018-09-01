1350198f2fe2145b11d22452c2eb94b45f7e27e3
Eoin Ó Conchúir <e@eoinoc.net>
2018-09-01 21:33:36 +0100

    Use local API for local development

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const local = process.env.NODE_ENV !== "production";
+const baseUrl = local
+  ? "http://192.168.56.101:8383"
+  : "https://api.bitesize.irish";
+
 export default axios.create({
-  baseURL: "https://api.bitesize.irish",
+  baseURL: baseUrl,
   headers: {
     // 'X-Auth-Token': 'f2b6637ddf355a476918940289c0be016a4fe99e3b69c83d',
     Authorization: "Bearer {token}",
