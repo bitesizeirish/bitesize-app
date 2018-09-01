d055a620a8a1b4a4ecc6aec999b4fef592c6e662
Eoin Ó Conchúir <e@eoinoc.net>
2018-09-01 21:35:36 +0100

    Formatting

--- src/store.js
+++ src/store.js
@@ -1,18 +1,23 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import axios from "axios";
-import api from "./utils/api-client"
-import { AUTH_PENDING, AUTH_SUCCESS, AUTH_FAILED, LOGOUT } from './store/mutation-types';
+// import axios from "axios";
+import api from "./utils/api-client";
+import {
+  AUTH_PENDING,
+  AUTH_SUCCESS,
+  AUTH_FAILED
+  // LOGOUT
+} from "./store/mutation-types";
 
 Vue.use(Vuex);
 
-const debug = process.env.NODE_ENV !== 'production';
+const debug = process.env.NODE_ENV !== "production";
 
 export default new Vuex.Store({
   state: {
-    status: '',
-    token: localStorage.getItem('token') || '',
-    user: null, // also seen initialised as {}...
+    status: "",
+    token: localStorage.getItem("token") || "",
+    user: null // also seen initialised as {}...
   },
   getters: {
     authStatus: state => state.status,
@@ -21,22 +26,23 @@ export default new Vuex.Store({
   mutations: {
     // ES2015 computed property name feature
     [AUTH_PENDING](state) {
-      state.status = 'pending';
+      state.status = "pending";
     },
     [AUTH_SUCCESS](state, token, user) {
-      state.status = 'success';
+      state.status = "success";
       state.token = token;
       state.user = user;
     },
     [AUTH_FAILED](state) {
-      state.status = 'failed';
+      state.status = "failed";
     }
   },
   actions: {
     login({ commit }, credentials) {
       return new Promise((resolve, reject) => {
         commit(AUTH_PENDING);
-        api.post("/auth/token", credentials)
+        api
+          .post("/auth/token", credentials)
           .then(response => {
             commit(AUTH_SUCCESS, response.data.token, response.data.user);
             resolve();
@@ -46,8 +52,7 @@ export default new Vuex.Store({
             commit(AUTH_FAILED);
             reject(error);
           });
-        }
-      );
+      });
     }
   },
   strict: debug
