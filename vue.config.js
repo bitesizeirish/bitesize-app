f0d2a4febc0b39137fa2bec9d50f0fb52c1ae549
Eoin Ó Conchúir <e@eoinoc.net>
2018-09-11 06:11:31 +0100

    Configure for Amazon S3 deployment

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,19 @@ const path = require('path');
 
 module.exports = {
   pluginOptions: {
-    "style-resources-loader": {
-      preProcessor: "scss",
-      patterns: [path.resolve(__dirname, "src/assets/custom.scss")]
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        '/mnt/data/projects/bitesize-app/src/assets/custom.scss'
+      ]
+    },
+    s3Deploy: {
+      assetPath: 'dist',
+      bucket: 'bitesize-web-app',
+      region: 'eu-west-1',
+      pwa: false,
+      enableCloudfront: false,
+      uploadConcurrency: 5
     }
   },
   configureWebpack: {
